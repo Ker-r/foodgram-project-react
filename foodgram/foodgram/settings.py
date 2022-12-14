8c5b42390bf7536ec382c4644861159f678007f4
Каролина Ефремова <efremova.karolina@gmail.com>
2022-12-14 20:27:07 +0700

    Вторая часть

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -154,8 +154,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-MEDIA_URL = '/dj_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'dj_media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
