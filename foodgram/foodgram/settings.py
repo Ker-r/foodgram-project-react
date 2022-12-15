71b63e80311b4fa2a2953c108043de78ad85f675
Каролина Ефремова <efremova.karolina@gmail.com>
2022-12-15 16:41:57 +0700

    Вторая часть

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.18.236', 'backend', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
