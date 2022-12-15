5dc7241d8d2dd6e5050c0d852ced888d4d063dec
Каролина Ефремова <efremova.karolina@gmail.com>
2022-12-15 17:05:05 +0700

    Вторая часть

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY', default='django-insecure-(bs666-k68iz@_t@x&q_$kprymtbyo+w6cs(qt&w2zkam3ot6f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
