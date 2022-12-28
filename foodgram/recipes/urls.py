4911323393c3e105a3c99a71141df360ee175705
Каролина Ефремова <efremova.karolina@gmail.com>
2022-12-28 22:55:21 +0700

    Вторая часть

--- foodgram/recipes/urls.py
+++ foodgram/recipes/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (
-    DownloadShop, FavoriteApiView, IngredientViewSet,
-    RecipeViewSet, ShopViewSet, TagViewSet
+    DownloadShop, IngredientViewSet,
+    RecipeViewSet, TagViewSet
 )
 
 
@@ -15,6 +15,4 @@ router.register('tags', TagViewSet, basename='tags')
 urlpatterns = [
     path('recipes/download_shopping_cart/', DownloadShop.as_view()),
     path('', include(router.urls)),
-    path('recipes/<int:favorite_id>/favorite/', FavoriteApiView.as_view()),
-    path('recipes/<int:recipe_id>/shopping_cart/', ShopViewSet.as_view()),
 ]
