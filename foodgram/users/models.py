6905ed096146f00959ec4f69ef3466087da03833
Каролина Ефремова <efremova.karolina@gmail.com>
2023-01-09 15:19:18 +0700

    Вторая часть

--- foodgram/users/models.py
+++ foodgram/users/models.py
@@ -2,63 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    USER = 'user'
+class CustomUser(AbstractUser):
+    email = models.EmailField('email', null=False, unique=True)
     MODERATOR = 'moderator'
     ADMIN = 'admin'
-    ROLES = {
-        (USER, 'user'),
-        (MODERATOR, 'moderator'),
-        (ADMIN, 'admin'),
-    }
-    email = models.EmailField(
-        verbose_name='Адрес email',
-        max_length=254,
-        unique=True,
-        blank=False,
-        help_text='Укажите свой email',
-    )
-    username = models.CharField(
-        verbose_name='Логин',
-        max_length=150,
-        unique=True,
-        help_text='Укажите свой никнейм',
-    )
-    first_name = models.CharField(
-        verbose_name='Имя',
-        max_length=150,
-        blank=True,
-        help_text='Укажите своё имя',
-    )
-    last_name = models.CharField(
-        verbose_name='Фамилия',
-        max_length=150,
-        blank=True,
-        help_text='Укажите свою фамилию',
-    )
-    role = models.CharField(
-        verbose_name='статус',
-        max_length=20,
-        choices=ROLES,
-        default=USER,
-    )
-    date_joined = models.DateTimeField(
-        verbose_name='Дата регистрации',
-        auto_now_add=True,
-    )
-    password = models.CharField(
-        verbose_name='Пароль',
-        max_length=150,
-        help_text='Введите пароль',
-    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     class Meta:
-        swappable = 'AUTH_USER_MODEL'
         verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        ordering = ['id']
 
     def __str__(self):
-        return self.get_full_name()
+        return f'Пользователь {self.email}'
 
     @property
     def is_moderator(self):
@@ -67,6 +23,9 @@ class User(AbstractUser):
     @property
     def is_admin(self):
         return self.is_superuser or self.role == self.ADMIN
+
+
+User = CustomUser
 
 
 class Follow(models.Model):
