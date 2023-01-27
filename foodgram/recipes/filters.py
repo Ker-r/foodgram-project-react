bea5f895ac5a8b7a9b6492e8912c7cd4cb7b3996
Каролина Ефремова <efremova.karolina@gmail.com>
2023-01-27 14:14:19 +0700

    commit

--- foodgram/recipes/filters.py
+++ foodgram/recipes/filters.py
@@ -1,13 +1,19 @@
-from django_filters import rest_framework as filters
+from django.contrib.auth import get_user_model
+from django_filters.rest_framework import FilterSet, filters
 
 from .models import Ingredient, Recipe, Tag
 
+User = get_user_model()
 
-class RecipeFilter(filters.FilterSet):
+
+class RecipeFilter(FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         queryset=Tag.objects.all(),
         to_field_name='slug',
+    )
+    author = filters.ModelChoiceFilter(
+        queryset=User.objects.all(),
     )
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
@@ -19,17 +25,19 @@ class RecipeFilter(filters.FilterSet):
         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
-        if value:
-            return queryset.filter(in_favorite__user=self.request.user)
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(in_favorite__user=user)
         return queryset
 
-    def filter_is_in_shopping_cart(self, queryset):
-        if self.request.query_params.get('is_in_shopping_cart'):
-            return queryset.filter(is_in_shopping_cart=True)
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and user.is_authenticated:
+            return queryset.filter(shopping_recipe__user=user)
         return queryset
 
 
-class IngredientFilter(filters.FilterSet):
+class IngredientFilter(FilterSet):
     name = filters.CharFilter(field_name='name', lookup_expr='icontains')
 
     class Meta:
